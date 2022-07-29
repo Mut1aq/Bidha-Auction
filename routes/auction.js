2e926361393b2f0c3d74252d7f41c99f49873273
Mutlaq <76498218+MutlaqFaharan@users.noreply.github.com>
2022-07-29 15:58:55 +0300

    Auction/Bid

--- routes/auction.js
+++ routes/auction.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.get("/", auctionController.getIndex);
 
+router.get("/bid", auctionController.getAuctionBid);
+
 router.get("/info", auctionController.getAuctionInfo);
 
 router.get("/car-info", auctionController.getCarInfo);
